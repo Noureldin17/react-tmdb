0819b81b7493e67d0c0fbde2bc5a261a93e052eb
Noureldin17 <noureldinn.nm@gmail.com>
2023-12-31 18:44:38 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: {
+        screen: '100dvh',
+      },
+      colors: {
+        primaryDark: '#080e12FF',
+        secondaryDark: '#131821FF',
+        primaryRed: '#5f1916FF',
+        linkPrimary: '#e6f0ffFF',
+        redButtonHover: '#68191dFF',
+        primaryBlue: '#369BFF',
+        primaryPurple: '#a4a4cc',
+        primaryGray: '#6A707C',
+        primarySilver: '#8391A1',
+      },
+    },
+  },
+  plugins: [],
+};
